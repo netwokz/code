e63eb3a5e9c693ab6c585716246b12d31b3336bf
Steve Deane Jr <wildmechanic@gmail.com>
2022-09-19 02:53:53 -0700

    fixes

--- site-checker/main/__main__.py
+++ site-checker/main/__main__.py
@@ -1,6 +1,8 @@
 import pathlib
 import sys
-from cli import read_cli_args
+from cli import read_cli_args, display_check_result
+from site_checker import site_is_online, site_is_online_async
+import asyncio
 
 def main():
     user_args = read_cli_args()
@@ -8,7 +10,10 @@ def main():
     if not urls:
         print("Error: no URLs to check", file=sys.stderr)
         sys.exit(1)
-    _synchronous_check(urls)
+    if user_args.asynchronous:
+        asyncio.run(_asynchronous_check(urls))
+    else:
+        _synchronous_check(urls)
 
 def _get_websites_urls(user_args):
     urls = user_args.urls
@@ -21,3 +26,34 @@ def _read_urls_from_file(file):
     if file_path.is_file():
         with file_path.open() as urls_file:
             urls = [url.strip() for url in urls_file]
+            if urls:
+                return urls
+            print(f"Error: empty input file, {file}", file=sys.stderr)
+    else:
+        print("Error: input file not found", file=sys.stderr)
+    return []
+
+async def _asynchronous_check(urls):
+    async def _check(url):
+        error = ""
+        try:
+            result = await site_is_online_async(url)
+        except Exception as e:
+            result = False
+            error = str(e)
+        display_check_result(result, url, error)
+
+    await asyncio.gather(*(_check(url) for url in urls))
+
+def _synchronous_check(urls):
+    for url in urls:
+        error = ""
+        try:
+            result = site_is_online(url)
+        except Exception as e:
+            result = False
+            error = str(e)
+        display_check_result(result, url, error)
+
+if __name__ == "__main__":
+    main()
